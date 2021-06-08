f5fd882daf47982ee4735217d9bece3d9a317e03
Divin Irakiza <irakizadivin@gmail.com>
2021-06-08 09:00:04 +0200

    modified default.vue

---
+++ components/reusable/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="header">
+    <div class="logo">
+      <nuxt-link to="/">
+        <Logo />
+      </nuxt-link>
+    </div>
+    <nav class="nav">
+      <div class="nav__link">
+        <nuxt-link to="/">Home</nuxt-link>
+      </div>
+      <div class="nav__link">
+        <nuxt-link to="/About">About</nuxt-link>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+    import Logo from "@/components/Logo.vue";
+    export default {
+        name: "nav-bar",
+        components: {
+            Logo
+        }
+    };
+</script>
+
+
+<style>
+</style>
