b2a82d26c0c672b14ac54dbe02f5be2dd98f47ff
Divin Irakiza <irakizadivin@gmail.com>
2021-06-08 11:19:55 +0200

    scss flex usage

---
+++ pages/flex.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="container-1">
+            <div class="box-1">
+                <h3>Box One</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. ?</p>
+            </div>
+            <div class="box-2">
+                <h3>Box Two</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. ?</p>
+            </div>
+            <div class="box-3">
+                <h3>Box Three</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. ?</p>
+            </div>
+        </div>
+
+
+        <div class="container-2">
+            <div class="container-2-box">
+                <h3>Box One</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. ?</p>
+            </div>
+            <div class="container-2-box">
+                <h3>Box Two</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. ?</p>
+            </div>
+            <div class="container-2-box">
+                <h3>Box Three</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. ?</p>
+            </div>
+        </div>
+    </div>
+</template>
+
